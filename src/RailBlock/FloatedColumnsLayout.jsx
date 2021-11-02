99f3b1c057a450d6f3b8516a2156b0ea179df7ee
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-11-02 13:58:30 +0200

    Improve floated layout

--- src/RailBlock/FloatedColumnsLayout.jsx
+++ src/RailBlock/FloatedColumnsLayout.jsx
@@ -7,26 +7,42 @@ import cx from 'classnames';
 const FloatedColumnsLayout = ({ data, metadata, path, content }) => {
   const blocks = getBlocks(data);
   const { mainColumnIndex } = data;
+  const [mainBlockId, mainBlock] = blocks[mainColumnIndex];
 
-  return blocks.map(([blockId, blockData], index) =>
-    blockHasValue(blockData) ? (
+  return (
+    <>
+      {blocks
+        .filter((b, i) => i !== mainColumnIndex)
+        .map(([blockId, blockData], index) =>
+          blockHasValue(blockData) ? (
+            <RenderBlock
+              metadata={metadata}
+              path={path}
+              content={content}
+              key={blockId}
+              data={blockData}
+              block={blockId}
+              as={index === mainColumnIndex ? React.Fragment : 'div'}
+              wrapperProps={{
+                className: cx('ui', {
+                  left: index < mainColumnIndex,
+                  right: index > mainColumnIndex,
+                  floated: index < mainColumnIndex || index > mainColumnIndex,
+                }),
+              }}
+            />
+          ) : null,
+        )}
+
       <RenderBlock
         metadata={metadata}
         path={path}
         content={content}
-        key={blockId}
-        data={blockData}
-        block={blockId}
-        as={index === mainColumnIndex ? React.Fragment : 'div'}
-        wrapperProps={{
-          className: cx('ui', {
-            left: index < mainColumnIndex,
-            right: index > mainColumnIndex,
-            floated: index < mainColumnIndex || index > mainColumnIndex,
-          }),
-        }}
+        data={mainBlock}
+        block={mainBlockId}
+        as={React.Fragment}
       />
-    ) : null,
+    </>
   );
 };
 
